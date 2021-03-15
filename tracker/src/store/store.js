4f0b87b9dc2e0930cdee05016e68adfa06f7ed8d
Hanna Sachok <hanna.sachok@gmail.com>
2021-03-15 14:51:59 +0200

    Debug fixing

--- tracker/src/store/store.js
+++ tracker/src/store/store.js
@@ -2,9 +2,9 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     rootReducer,
-    compose(applyMiddleware(thunk), devTools)
+    composeEnhancer(applyMiddleware(thunk))
 );
